247468b6d1a3788d86084bdee70aa4ce6d607aa4
HM Tanbir <tanbir2025@gmail.com>
2022-09-08 22:10:14 +0600

    update query type

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -6,14 +6,24 @@ module Types
 
     # Add root-level fields here.
     # They will be entry points for queries on your schema.
+
     field :items, [Types::ItemType], null: false, description: 'Return a list of items'
-    field :artists, [Types::ArtistType], null: false, description: 'Return a list of artist'
-    field :artist, Types::ArtistType, null: false do
+    field :item, Types::ItemType, null: false do
       argument :id, ID, required: true
     end
 
     def items
       Item.all
+    end
+
+    def item(id:)
+      Item.find_by(id: id)
+    end
+
+    # Artist
+    field :artists, [Types::ArtistType], null: false, description: 'Return a list of artist'
+    field :artist, Types::ArtistType, null: false do
+      argument :id, ID, required: true
     end
 
     def artists
